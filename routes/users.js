778ccdd3015a2d29b597c5697068e8ef93ea8ced
Your Name <you@example.com>
2023-03-24 21:36:06 +0300

    00

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express =require('express');
+const router= express.Router();
+const User = require('../models/User');
+//@route   POST api/users
+//@desc    Register a user
+//@access  Publicdsdsdsd
+
+router.post('/' , (req , res)=>{res.send(req.body)});
+
+module.exports= router;
