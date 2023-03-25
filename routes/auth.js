abaec3c0d079f7ed09f7b656ff54d6fc0ffc2aaa
Your Name <you@example.com>
2023-03-26 00:35:45 +0300

    Authinticate Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { body, validationResult, check } = require("express-validator");
 //@route   GET api/auth
 //@desc    Get logged in user
 //@access  private
@@ -13,7 +16,61 @@ router.get("/", (req, res) => {
 //@desc    Auth user and get token
 //@access  public
 
-router.post("/", (req, res) => {
-  res.send("Auth user and get token ");
+router.post("/",
+ [
+  check('email' , 'Please include a valid email').isEmail(),
+  check('password' , 'Password is requierd').exists()
+
+ ] , 
+ async (req, res) =>
+ {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+    {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const{email ,password} =req.body;
+    try{
+
+      let user = await User.findOne({email});
+      if(!user)
+      {
+        return res.status(400).json({msg:'Invalid Credentials'})
+
+      }
+      
+      const isMatch= await bcrypt.compare(password,user.password);
+      if(!isMatch)
+      {
+        return res.status(400).json({msg:'Invalid Credentials'});
+      }
+
+      //jwt
+      const payload =
+      {
+        user:
+        {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) =>
+        {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) { 
+      
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+
 });
 module.exports = router;
