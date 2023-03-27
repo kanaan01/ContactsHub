8efb7808e28496fb4a87c47df8284ea28a009273
Your Name <mkanaan0502@gmail.com>
2023-03-27 22:04:04 +0300

    Adding Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,34 +1,56 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
-const {check , validationResult} = require('express-validator');
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-const User = require('../models/User');
-const contact = require('../models/Contact');
+const User = require("../models/User");
+const contact = require("../models/Contact");
 const Contact = require("../models/Contact");
 //@route   POST  api/contacts
 //@desc    Add new contacts
 //@access  private
 
-router.post("/", (req, res) => {
-  res.send("Add contact");
-});
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const [name, email, phone, type] = req.body;
+    try {
+      const newcontact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(er.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 //@route   GET  api/contacts
 //@desc    Get all users contacts(yours)
 //@access  private
 
-router.get("/", auth , async (req, res) => {
-
-   try {
-      const contacts = await Contact.find({user: req.user.id}).sort({date:-1});
-      res.json(contacts);
-   } catch (err) {
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
     console.error(err.message);
-    res.status(400).send('Server Error');
-    
-   }
- 
+    res.status(400).send("Server Error");
+  }
 });
 
 //@route   PUT  api/contacts/:id
