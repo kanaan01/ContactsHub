b62751102160033ab8d9ee53eccf21a7b60ab8b9
Your Name <mkanaan0502@gmail.com>
2023-03-29 04:14:07 +0300

    client update

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+const App = () => {
+  return (
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route exact path="/about" Component={About} />
+          </Routes>
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
